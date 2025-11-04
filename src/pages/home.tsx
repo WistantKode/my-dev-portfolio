f38f6ebae38ec2ff45103ba27be4c27c81841f85
WistantKode <wistantkode@protonmail.com>
2025-11-04 07:06:47 +0100

    feat/about-part: create and edit my-experience file. J'ai repris le code de la partir de la page about ou il y avait mes experiences scolaires. Jen fais un composent minimaliste que je vais arranger plutard

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,16 +4,16 @@ import AboutSection from "@/components/home/about-section";
 import ServicesSection from "@/components/home/services-section";
 
 const Home = () => {
-  return (
-    <Layout 
-      title = "Modjo victor- Développeur Web Java React & IT student"
-      description = "Portfolio de Modjo victor, développeur web, mobile, desktop spécialisé dans la création de solutions digitales innovantes et performantes."
-      keywords = "développeur web, dévellopeur backend, Java, React, développement web, portfolio, Modjo Victor, solutions digitales">
-      <HeroSection />
-      <AboutSection />
-      <ServicesSection />
-    </Layout>
-  );
+    return (
+        <Layout
+            title="Modjo victor- Développeur Web Java React & IT student"
+            description="Portfolio de Modjo victor, développeur web, mobile, desktop spécialisé dans la création de solutions digitales innovantes et performantes."
+            keywords="développeur web, dévellopeur backend, Java, React, développement web, portfolio, Modjo Victor, solutions digitales">
+            <HeroSection/>
+
+            <ServicesSection/>
+        </Layout>
+    );
 };
 
 export default Home;
