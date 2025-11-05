530b60c1c1c391fbd0d349abd3b181900aa998e8
WistantKode <wistantkode@protonmail.com>
2025-11-05 06:55:03 +0100

    feat(home): Update title for clarity and branding in Home component

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,17 +1,15 @@
 import Layout from "@/components/layout/layout";
 import HeroSection from "@/components/home/HeroSection.tsx";
-import AboutSection from "@/components/home/AboutSection.tsx";
 import ServicesSection from "@/components/home/ServicesSection.tsx";
 import AboutMe from "@/components/AboutMe.tsx";
 import MyExperience from "@/components/MyExperience.tsx";
 import MyStack from "@/components/MyStack.tsx";
-import CTA from "@/components/CTA.tsx";
 import ContactForm from "@/components/ContactForm.tsx";
 
 const Home = () => {
     return (
         <Layout
-            title="Wistant Kode - DevSecOps Practicer | Software Engineering Student"
+            title="Wistant Kode | Porfolio | Software Engineering Student"
             description="Wistant Kode's portfolio: a passionate Software Engineering Student and DevSecOps Practicer. Specializing in Java/Spring Boot, React/Next.js, Cloud, Automation, and Cybersecurity. Building innovative and scalable digital solutions."
             keywords="Wistant Kode, DevSecOps, Software Engineer, Java, Spring Boot, React, Next.js, Cloud, Automation, Cybersecurity, Portfolio, Web Development, Backend Development, Africa Tech"
             lang="en"
